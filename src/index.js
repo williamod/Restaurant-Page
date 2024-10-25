e11a39ba8a525e685b6ecb3448e41b5077a3eea2
williamod <williamodonnell016@gmail.com>
2024-10-25 15:44:18 -0700

    Add webpack fil config

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import './style.css';
+import {test} from './home.js';
+import {test0} from './menu.js';
+import {test1} from './aboutUs.js';
+
+
+console.log(test);
+console.log(test0);
+console.log(test1);
---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+    mode: 'development',
+    entry: './src/index.js',
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'dist'),
+        clean: true,
+    },
+    devtool: 'eval-source-map',
+    devServer: {
+        watchFiles: ['./src/template.html'],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/template.html',
+        }),
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.html$/i,
+                loader: 'html-loader',
+            },
+            {
+                test: /\.(pgn|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+            },
+        ],
+    },
+};
